5c9aef719185dc3e41eaf8e915cfd05935b1ec00
Walter Lopes <walter_vinicius@outlook.com>
2020-04-12 16:26:18 -0300

    added new project to samples

---
+++ samples/Aloha.Services.Products/Aloha.Services.Products/Startup.cs
@@ -0,0 +1,58 @@
+﻿using Aloha.RabbitMQ.Messaging;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Aloha.Services.Products
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var configSection = Configuration.GetSection("RabbitMQ");
+            string host = configSection["Host"];
+            string userName = configSection["UserName"];
+            string password = configSection["Password"];
+
+            services.AddTransient<IMessagePublisher>((sp) => new RabbitMQMessagePublisher(host, userName, password, "aloha"));
+
+            services.AddTransient<IMessageHandler>((svc) =>
+            {
+                var rabbitMQConfigSection = Configuration.GetSection("RabbitMQ");
+                string rabbitMQHost = rabbitMQConfigSection["Host"];
+                string rabbitMQUserName = rabbitMQConfigSection["UserName"];
+                string rabbitMQPassword = rabbitMQConfigSection["Password"];
+                return new RabbitMQMessageHandler(rabbitMQHost, rabbitMQUserName, rabbitMQPassword, "Pitstop", "WorkshopManagement", "");
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
