bd4d48cc9d66d86f7f2ff41a23a86fc4f8299465
Walter Vinicius <walter.cardoso@socialminer.com>
2020-08-24 10:04:38 -0300

    Adding kinesis fiehose

---
+++ src/Aloha.Streams.AmazonKinesis/AmazonKinesisOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aloha.Streams.AmazonKinesis
+{
+    public class AmazonKinesisOptions
+    {
+        public string AccessKey { get; set; }
+
+        public string SecretKey { get; set; }
+
+        public string Region { get; set; }
+
+        public IDictionary<string, string> Queues { get; set; }
+    }
+}
